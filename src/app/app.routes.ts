e9aae301df01450b457450184baee59bf88df6fe
Mauricio González Cabrera <hello@mauro-gonzalez.dev>
2025-01-28 15:42:59 -0300

    feat: basic routing is created
    Basic routing is created with pages: home, about and projects.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/home-page/home-page.component').then(
+        (m) => m.HomePageComponent
+      );
+    },
+  },
+  {
+    path: 'about',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/about-page/about-page.component').then(
+        (m) => m.AboutPageComponent
+      );
+    },
+  },
+  {
+    path: 'projects',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/projects-page/projects-page.component').then(
+        (m) => m.ProjectsPageComponent
+      );
+    },
+  },
+  {
+    path: '**',
+    loadComponent: () => {
+      return import('./pages/error-page/error-page.component').then(
+        (m) => m.ErrorPageComponent
+      );
+    },
+  },
+];
