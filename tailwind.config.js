d15841ce738f38934968d0250b003e5482f44db8
Mauricio González Cabrera <hello@mauro-gonzalez.dev>
2025-02-02 16:29:47 -0300

    feat: added fira code font family & favicon
    Added fira code font family & favicon, also reorganized navbar icons and assets.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "fira-code": ["Fira Code", "serif"],
+      },
+    },
   },
   plugins: [],
 };
