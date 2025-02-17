151b3d0d1ef27d510d7880672f4a84d72ddcaf0f
Mauricio González Cabrera <hello@mauro-gonzalez.dev>
2025-02-17 20:03:26 -0300

    feat: added title component
    Added title component & home-page behavior.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../components/title/title.component';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './home-page.component.html',
   styles: ``,
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  private _title: string = 'Mauricio González';
+  private _subtitle: string = 'Professional Web Developer';
+
+  get getTitle(): string {
+    return this._title;
+  }
+
+  get getSubtitle(): string {
+    return this._subtitle;
+  }
+}
