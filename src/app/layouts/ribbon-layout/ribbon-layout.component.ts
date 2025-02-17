1b11a694ca32fa725cb8333275cb8641ad6ff7a3
Mauricio González Cabrera <hello@mauro-gonzalez.dev>
2025-02-17 17:20:05 -0300

    feat: added ribbon behavior & site legend
    Added ribbon and legend behavior at the bottom ribbon.

--- src/app/layouts/ribbon-layout/ribbon-layout.component.ts
+++ src/app/layouts/ribbon-layout/ribbon-layout.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FullScreenComponent } from '../../components/full-screen/full-screen.component';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroHeart } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-ribbon-layout',
-  imports: [FullScreenComponent],
+  standalone: true,
+  imports: [FullScreenComponent,NgIcon],
   templateUrl: './ribbon-layout.component.html',
-  styles: ``
+  providers: [
+    provideIcons({
+        heroHeart
+      })
+  ],
 })
 export class RibbonLayoutComponent {
+  @Input()
+  set setPosition(position: 'top' | 'bottom') {
+    if (!position) {
+      return;
+    }
+    this._position = position;
+  }
 
+  private _position: 'top' | 'bottom' = 'top';
+
+  get getPosition(): 'top' | 'bottom' {
+    return this._position;
+  }
 }
